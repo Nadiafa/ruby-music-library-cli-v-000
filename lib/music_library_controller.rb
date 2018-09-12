2a49a2caa2b14be7deb16cefc8b748789f34893a
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-12 15:40:53 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -6,12 +6,7 @@ class MusicLibraryController
 
   def call
     puts "Welcome to your music library!"
-
     input = ""
-
-    system "clear"
-    input = nil
-
     while input != "exit"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
